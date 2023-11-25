722af9694c0d067d579cf2a5456564f0f855d948
ieeemma <emma.freeman@protonmail.com>
2023-11-25 14:41:37 +0000

    feat(parser): source location tracking

--- src/Parse/Parse.hs
+++ src/Parse/Parse.hs
@@ -1,16 +1,29 @@
 module Parse.Parse where
 
 import Parse.Lex
-import Parse.Stream (TokenStream)
+import Parse.Stream
 import Syntax
 
+import Control.Comonad.Cofree (Cofree (..))
 import Control.Monad.Combinators.Expr (Operator (..), makeExprParser)
 import Data.Text (Text, unpack)
 import Data.Void (Void)
 import Text.Megaparsec hiding (tokens)
+import Prelude hiding (pi)
 
 -- | Parser over the custom `TokenStream` type.
 type Parser = Parsec Void TokenStream
+
+-- | Source location tracking
+data Span = Span SourcePos SourcePos
+
+-- | Wrap a parser in its source location span using a `Cofree`.
+spanned :: Parser (f (Cofree f Span)) -> Parser (Cofree f Span)
+spanned p = do
+  l <- getSourcePos
+  x <- p
+  r <- getSourcePos
+  pure (Span l r :< x)
 
 -- | Parse a token of a given kind.
 tok :: TokenKind -> Parser Text
@@ -28,50 +41,56 @@ block :: Parser a -> Parser [a]
 block p = tok TOpen *> p `sepBy` tok TSep <* tok TClose
 
 -- | Parse a whole term with operator precedence.
-tm :: Parser Tm
+tm :: Parser (ATm Span)
 tm =
   -- TODO: custom operator precedence for terms and patterns.
-  makeExprParser
-    atom
-    [ [InfixL $ pure App]
-    , [InfixR $ Pi "_" <$ tok TArrow]
-    ]
+  makeExprParser grouping [[InfixL (pure app)], [InfixR (pure arr)]]
+ where
+  app x@(Span l _ :< _) y@(Span _ r :< _) = Span l r :< AppF x y
+  arr x@(Span l _ :< _) y@(Span _ r :< _) = Span l r :< PiF "_" x y
+
+grouping :: Parser (ATm Span)
+grouping = spanned pi <|> paren <|> atom
+ where
+  pi =
+    PiF
+      <$> (tok TLParen *> tok TLower)
+      <*> (tok TColon *> tm <* tok TRParen)
+      <*> (tok TArrow *> tm)
+  paren = tok TLParen *> tm <* tok TRParen
 
 -- | Parse an atomic term.
-atom :: Parser Tm
+atom :: Parser (ATm Span)
 atom =
-  choice
-    [ Pi
-        <$> (tok TLParen *> tok TLower)
-        <*> (tok TColon *> tm <* tok TRParen)
-        <*> (tok TArrow *> tm)
-    , tok TLParen *> tm <* tok TRParen
-    , Lam
-        <$> (tok TLambda *> tok TLower)
-        <*> (tok TArrow *> tm)
-    , Let
-        <$> (tok TLet *> block bind)
-        <*> (tok TIn *> tm)
-    , Case
-        <$> (tok TCase *> tm)
-        <*> (tok TOf *> block alt)
-    , Lit <$> int
-    , U <$ tok TType
-    , Sym <$> tok TLower
-    , Con <$> tok TUpper
-    ]
+  spanned
+    $ choice
+      [ LamF
+          <$> (tok TLambda *> tok TLower)
+          <*> (tok TArrow *> tm)
+      , LetF
+          <$> (tok TLet *> block bind)
+          <*> (tok TIn *> tm)
+      , CaseF
+          <$> (tok TCase *> tm)
+          <*> (tok TOf *> block alt)
+      , LitF <$> int
+      , UF <$ tok TType
+      , SymF <$> tok TLower
+      , ConF <$> tok TUpper
+      ]
  where
   bind = (,,) <$> tok TLower <*> (tok TColon *> tm) <*> (tok TEquals *> tm)
   alt = (,) <$> pat <*> (tok TArrow *> tm)
 
 -- | Parse a pattern.
-pat :: Parser Pat
+pat :: Parser (APat Span)
 pat =
-  choice
-    [ Destruct
-        <$> tok TUpper
-        <*> many pat
-    , Bind <$> tok TLower
-    , IsLit <$> int
-    , Wild <$ tok TUscore
-    ]
+  spanned
+    $ choice
+      [ DestructF
+          <$> tok TUpper
+          <*> many pat
+      , BindF <$> tok TLower
+      , IsLitF <$> int
+      , WildF <$ tok TUscore
+      ]
