a6ad0fe7c6901a755939b6c0caf719a34ea954fa
ieeemma <emma.freeman@protonmail.com>
2023-12-02 17:03:45 +0000

    refactor(pretty): rewrite for better precedence handling

--- src/Pretty.hs
+++ src/Pretty.hs
@@ -1,71 +1,55 @@
-{-# LANGUAGE UndecidableInstances #-}
-
 module Pretty where
 
-import Control.Arrow ((>>>))
-import Control.Comonad.Trans.Cofree (CofreeF (..))
-import Data.Functor.Foldable (Base, Recursive, para, project)
-import Prettyprinter (Doc, hsep, indent, line, pretty, vsep, (<+>))
+import Control.Applicative (liftA2)
+import Data.Functor.Foldable (para)
+import Prettyprinter (Doc, Pretty (..), hsep, indent, line, vsep, (<+>))
+
 import Syntax
 
-class Render f where
-  -- | Given a structure containing `Doc`s, render it to a `Doc`.
-  step :: f (Doc ann) -> Doc ann
-
-  -- | Return the precedence of a structure for parenthesization.
-  prec :: f a -> Int
-
--- | Render a `Render`able structure to a `Doc`, inserting parentheses.
-render :: (Recursive a, Render (Base a)) => a -> Doc ann
-render = para (parens >>> step)
- where
-  parens x = paren (prec x) <$> x
-  paren n (y, d)
-    | prec (project y) > n = "(" <> d <> ")"
-    | otherwise = d
-
--- instance {-# OVERLAPS #-} (Recursive a, Render (Base a)) => Show a where
---   show = show . render
-
-instance (Render f) => Render (CofreeF f a) where
-  step (_ :< xs) = step xs
-  prec (_ :< xs) = prec xs
-
-instance (Recursive p, Render (Base p)) => Render (TmF p) where
-  step = \case
-    PiF "_" σ π -> σ <+> "->" <+> π
-    PiF x σ π -> "(" <> pretty x <> ":" <+> σ <> ")" <+> "->" <+> π
-    LamF x e -> "λ" <> pretty x <+> "->" <+> e
-    AppF e₁ e₂ -> e₁ <+> e₂
-    LetF bs e -> "let" <+> block (bind <$> bs) <+> "in" <+> e
-    CaseF e ps -> "case" <+> e <+> "of" <+> block (alt <$> ps)
+instance (Pretty p) => Pretty (Tm p) where
+  pretty = para \case
+    PiF "_" σ π -> paren atom σ <+> "->" <+> paren (liftA2 (||) atom arrow) π
+    PiF x (_, σ) (_, π) -> "(" <> pretty x <+> ":" <+> σ <> ")" <+> "->" <+> π
+    LamF x (_, e) -> "λ" <> pretty x <+> "->" <+> e
+    AppF e₁ e₂ -> paren (liftA2 (||) atom app) e₁ <+> paren atom e₂
+    LetF bs (_, e) -> "let" <+> block (bind <$> bs) <+> "in" <+> e
+    CaseF (_, e) ps -> "case" <+> e <+> "of" <+> block (alt <$> ps)
     SymF x -> pretty x
     ConF x -> pretty x
     LitF n -> pretty n
     UF -> "Type"
    where
-    bind (x, σ, e) = pretty x <> ":" <+> σ <+> "=" <+> e
-    alt (p, e) = render p <+> "->" <+> e
+    bind (x, (_, σ), (_, e)) = pretty x <> ":" <+> σ <+> "=" <+> e
+    alt (p, (_, e)) = pretty p <+> "->" <+> e
 
-  prec = \case
-    PiF "_" _ _ -> 10
-    PiF{} -> 2
-    LamF{} -> 10
-    AppF{} -> 1
-    LetF{} -> 10
-    CaseF{} -> 10
-    _ -> 0
+    arrow = \case
+      Pi "_" _ _ -> True
+      _ -> False
+    app = \case
+      App{} -> True
+      _ -> False
+    atom = \case
+      Pi{} -> False
+      Lam{} -> False
+      App{} -> False
+      Let{} -> False
+      Case{} -> False
+      _ -> True
 
-instance Render PatF where
-  step = \case
-    DestructF x ps -> pretty x <+> hsep ps
+instance Pretty Pat where
+  pretty = para \case
+    DestructF x ps -> pretty x <+> hsep (paren atom <$> ps)
     BindF x -> pretty x
     IsLitF n -> pretty n
     WildF -> "_"
+   where
+    atom = \case
+      Destruct{} -> False
+      _ -> True
 
-  prec = \case
-    DestructF{} -> 10
-    _ -> 0
+-- | Parenthesize a `Doc` if a predicate is false.
+paren :: (a -> Bool) -> (a, Doc ann) -> Doc ann
+paren p (e, x) = if not (p e) then "(" <> x <> ")" else x
 
 -- | Line-separate and indent a list of `Doc`s.
 block :: [Doc ann] -> Doc ann
