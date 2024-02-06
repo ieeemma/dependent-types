a945e7c369c2f0302ace0ba26fc28df958a35294
ieeemma <emma.freeman@protonmail.com>
2024-02-06 21:43:50 +0000

    refactor(infer): closure type parameter

--- src/Infer/Value.hs
+++ src/Infer/Value.hs
@@ -8,6 +8,7 @@ Type checking converts terms to values (`eval`) and values to terms (`quote`).
 -}
 module Infer.Value where
 
+import Control.Comonad.Cofree (Cofree)
 import Data.Functor.Foldable.TH (makeBaseFunctor)
 import Data.Map (Map)
 
@@ -18,10 +19,10 @@ import Syntax
 {- | Values mirror terms, but without `let` and `case`.
 Also stores closures on Π and λ.
 -}
-data Val
-  = VPi Sym Val Clos
-  | VLam Sym Clos
-  | VApp Val Val
+data Val c
+  = VPi Sym (Val c) c
+  | VLam Sym c
+  | VApp (Val c) (Val c)
   | VSym Sym
   | VCon Sym
   | VLit Int
@@ -32,9 +33,11 @@ This captures the scope of a function.
 For example, in `λx -> fx`, the closure captures the value of `f` as
 it is free.
 -}
-data Clos = forall a. Clos Env (Tm :@ a)
+data Clos a = Clos (Env a) (Tm :@ a)
 
 -- | Environments map symbols to values.
-type Env = Map Sym Val
+type Env a = Map Sym (Val :@ a)
 
 makeBaseFunctor ''Val
+
+type instance Val :@ a = Cofree (ValF (Clos a)) a
