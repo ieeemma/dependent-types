6d93e0bdd501e799091535b37e4a5f1566c62e02
ieeemma <emma.freeman@protonmail.com>
2024-04-15 23:41:11 +0100

    feat(compile): racket compilation

--- src/Main.hs
+++ src/Main.hs
@@ -7,8 +7,8 @@ import Data.Text.IO qualified as TIO
 import Error.Diagnose (addFile, addReport, def, defaultStyle, printDiagnostic, stderr)
 import Text.Megaparsec (errorBundlePretty, parse)
 
+import Compile.Compile (compile)
 import Infer.Infer (Ctx (..), infer)
-import Infer.Quote (quote)
 import Infer.Value
 import Parse.Parse (Span, file)
 import Parse.Pretty ()
@@ -24,4 +24,4 @@ main = do
     Left e -> putStrLn (errorBundlePretty e)
     Right t -> case runExcept (runReaderT (infer t) (Ctx tys mempty)) of
       Left e -> printDiagnostic stderr True True 4 defaultStyle (addReport (addFile def name (unpack src)) e)
-      Right v -> print (quote v)
+      Right _ -> TIO.putStrLn (compile t)
